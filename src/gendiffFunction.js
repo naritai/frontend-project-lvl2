44934c283d28750a49de56800abd806c428d9efa
leekar0 <leekar0@yandex.ru>
2022-06-08 16:33:21 +0300

    refactor naming

--- src/gendiffFunction.js
+++ src/gendiffFunction.js
@@ -1,13 +1,13 @@
 import parseFile from './parsers.js';
 import getFormatter from './formatters/index.js';
-import astBuilder from './astBuilder.js';
+import buildAST from './astBuilder.js';
 
 function genDiff(filepath1, filepath2, formatName = 'stylish') {
   const parsedObj1 = parseFile(filepath1);
   const parsedObj2 = parseFile(filepath2);
-  const ast = astBuilder(parsedObj1, parsedObj2);
-  const formatter = getFormatter(formatName);
-  const result = formatter(ast);
+  const ast = buildAST(parsedObj1, parsedObj2);
+  const format = getFormatter(formatName);
+  const result = format(ast);
   return result;
 }
 
